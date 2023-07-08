67f8ab89fab776578cae6dd46883ebd328641d48
sheraz <sherazzafar148@gmail.com>
2023-07-08 12:49:02 +0500

    ok

---
+++ pages/index.js
@@ -0,0 +1,311 @@
+import styles from "./index.module.css";
+const FrameComponent = () => {
+  return (
+    <div className={styles.rectangleParent}>
+      <div className={styles.frameChild} />
+      <div className={styles.bitech}>Bitech</div>
+      <div className={styles.homeAboutUsContainer}>
+        <span className={styles.home}>Home</span>
+        <span> About Us Our App Contact</span>
+      </div>
+      <div className={styles.login}>Login</div>
+      <div className={styles.rectangleGroup}>
+        <div className={styles.groupChild} />
+        <div className={styles.signUp}> Sign Up</div>
+      </div>
+      <div className={styles.wereADigitalContainer}>
+        <p className={styles.wereADigital}>We’re A Digital</p>
+        <p className={styles.wereADigital}>{`Software House `}</p>
+        <p className={styles.wereADigital}>Startup Business</p>
+      </div>
+      <div className={styles.ourServices}>{`Our Services & Our Goal`}</div>
+      <div className={styles.ourServices1}>{`Our Services & Our Goal`}</div>
+      <div className={styles.sendYourMessageContainer}>
+        <span>{`Send Your `}</span>
+        <span className={styles.messageToUs}>Message To Us</span>
+      </div>
+      <img className={styles.frameItem} alt="" src="/group-2.svg" />
+      <div className={styles.loremIpsumIs}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum
+      </div>
+      <div className={styles.rectangleContainer}>
+        <div className={styles.groupItem} />
+        <div className={styles.groupDiv}>
+          <div className={styles.groupInner} />
+          <div className={styles.rectangleDiv} />
+          <div className={styles.groupChild1} />
+          <div className={styles.groupChild2} />
+          <div className={styles.learnMore}>Learn More</div>
+          <div className={styles.sendMessage}>Send Message</div>
+          <div className={styles.learnMore1}>Learn More</div>
+          <div className={styles.learnMore2}>Learn More</div>
+        </div>
+        <div className={styles.learnMore3}>Learn More</div>
+      </div>
+      <div className={styles.services}>SERVICES</div>
+      <div className={styles.testimonials}>TESTIMONIALS</div>
+      <div className={styles.feelFreeToContactWithUsParent}>
+        <div className={styles.feelFreeToContainer}>
+          <p className={styles.wereADigital}>
+            <span className={styles.feel}>{`Feel `}</span>
+            <span>Free</span>
+          </p>
+          <p className={styles.wereADigital}>
+            <span>To Contact</span>
+            <span className={styles.feel}> With Us</span>
+          </p>
+        </div>
+        <div className={styles.contactUs}>Contact Us</div>
+      </div>
+      <div className={styles.services1}>SERVICES</div>
+      <div className={styles.rectangleParent1}>
+        <div className={styles.groupChild3} />
+        <div className={styles.groupChild4} />
+        <div className={styles.groupChild5} />
+        <div className={styles.groupChild6} />
+        <div className={styles.groupChild7} />
+        <div className={styles.groupChild8} />
+      </div>
+      <img className={styles.frameInner} alt="" src="/group-4.svg" />
+      <div className={styles.graphicDesign}>Graphic Design</div>
+      <div className={styles.development}>Development</div>
+      <div className={styles.logonDesignBusinessContainer}>
+        <p className={styles.wereADigital}>Logon Design</p>
+        <p className={styles.wereADigital}>Business Card</p>
+        <p className={styles.wereADigital}>Stationery</p>
+        <p className={styles.wereADigital}>Label Design</p>
+        <p className={styles.wereADigital}>Letterhead</p>
+        <p className={styles.wereADigital}>Billboard Design</p>
+        <p className={styles.wereADigital}>etc...</p>
+      </div>
+      <div className={styles.logonDesignBusinessContainer1}>
+        <p className={styles.wereADigital}>Logon Design</p>
+        <p className={styles.wereADigital}>Business Card</p>
+        <p className={styles.wereADigital}>Stationery</p>
+        <p className={styles.wereADigital}>Label Design</p>
+        <p className={styles.wereADigital}>Letterhead</p>
+        <p className={styles.wereADigital}>Billboard Design</p>
+        <p className={styles.wereADigital}>etc...</p>
+      </div>
+      <div className={styles.ellipseDiv} />
+      <div className={styles.frameChild1} />
+      <div className={styles.frameChild2} />
+      <div className={styles.frameChild3} />
+      <div className={styles.frameChild4} />
+      <div className={styles.frameChild5} />
+      <div className={styles.frameChild6} />
+      <div className={styles.frameChild7} />
+      <div className={styles.frameChild8} />
+      <div className={styles.frameChild9} />
+      <div className={styles.frameChild10} />
+      <div className={styles.frameChild11} />
+      <div className={styles.frameChild12} />
+      <div className={styles.frameChild13} />
+      <div className={styles.uiuxDesignParent}>
+        <div className={styles.uiuxDesign}>UI/UX Design</div>
+        <div className={styles.websiteDesignAppContainer}>
+          <p className={styles.wereADigital}>Website Design</p>
+          <p className={styles.wereADigital}>App Design</p>
+          <p className={styles.wereADigital}>User Stories</p>
+          <p className={styles.wereADigital}>User flow charts</p>
+          <p className={styles.wereADigital}>Wireframes</p>
+        </div>
+        <div className={styles.groupChild9} />
+        <div className={styles.groupChild10} />
+        <div className={styles.groupChild11} />
+        <div className={styles.groupChild12} />
+        <div className={styles.groupChild13} />
+      </div>
+      <img className={styles.vectorIcon} alt="" src="/vector.svg" />
+      <div className={styles.frameChild14} />
+      <div className={styles.innovativeWebsiteDesign}>
+        Innovative website Design
+      </div>
+      <div className={styles.uiuxDesignWith}>
+        Ui/Ux Design With Global Trend
+      </div>
+      <div className={styles.webAdnEmail}>Web adn email hosting serivce</div>
+      <div className={styles.growYourBusinessContainer}>
+        <p className={styles.wereADigital}>{`Grow Your Business With `}</p>
+        <p className={styles.wereADigital}>Our Agency</p>
+      </div>
+      <div className={styles.forwardThinkingTeamContainer}>
+        <p className={styles.wereADigital}>Forward Thinking Team Of</p>
+        <p className={styles.wereADigital}>{`Designers & Developers`}</p>
+      </div>
+      <div className={styles.forwardThinkingTeamContainer1}>
+        <p className={styles.wereADigital}>Forward Thinking Team Of</p>
+        <p className={styles.wereADigital}>{`Designers & Developers`}</p>
+      </div>
+      <div className={styles.loremIpsumIs1}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s
+      </div>
+      <div className={styles.loremIpsumIs2}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s
+      </div>
+      <div className={styles.loremIpsumIs3}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s
+      </div>
+      <div className={styles.aboutUs}>ABOUT US</div>
+      <div className={styles.ourCapabilities}>OUR CAPABILITIES</div>
+      <div className={styles.aboutUs1}>ABOUT US</div>
+      <img className={styles.groupIcon} alt="" src="/group.svg" />
+      <img className={styles.vectorIcon1} alt="" src="/vector1.svg" />
+      <img className={styles.vectorIcon2} alt="" src="/vector1.svg" />
+      <img className={styles.vectorIcon3} alt="" src="/vector1.svg" />
+      <div className={styles.frameChild15} />
+      <div className={styles.frameChild16} />
+      <div className={styles.frameChild17} />
+      <div className={styles.frameChild18} />
+      <div className={styles.div}>80%</div>
+      <div className={styles.div1}>90%</div>
+      <div className={styles.div2}>85%</div>
+      <div className={styles.div3}>100%</div>
+      <div className={styles.softwareDevelopment}>
+        <p className={styles.wereADigital}>{`Software `}</p>
+        <p className={styles.wereADigital}>Development</p>
+      </div>
+      <div className={styles.graphicDesign1}>Graphic Design</div>
+      <div className={styles.gameDesign}>Game Design</div>
+      <div className={styles.webDevelopment}>
+        <p className={styles.wereADigital}>{`Web `}</p>
+        <p className={styles.wereADigital}>Development</p>
+      </div>
+      <img className={styles.groupIcon1} alt="" src="/group1.svg" />
+      <div className={styles.frameChild19} />
+      <div className={styles.frameChild20} />
+      <div className={styles.frameChild21} />
+      <div className={styles.frameChild22} />
+      <div className={styles.frameChild23} />
+      <div className={styles.frameChild24} />
+      <div className={styles.frameChild25} />
+      <img className={styles.ellipseIcon} alt="" src="/ellipse-17@2x.png" />
+      <img className={styles.frameChild26} alt="" src="/ellipse-18@2x.png" />
+      <img className={styles.frameChild27} alt="" src="/ellipse-19@2x.png" />
+      <img className={styles.frameChild28} alt="" src="/group-9.svg" />
+      <img className={styles.frameChild29} alt="" src="/group-10.svg" />
+      <img className={styles.frameChild30} alt="" src="/group-10.svg" />
+      <div
+        className={styles.loremIpsumIs4}
+      >{`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard `}</div>
+      <div
+        className={styles.loremIpsumIs5}
+      >{`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard `}</div>
+      <div
+        className={styles.loremIpsumIs6}
+      >{`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard `}</div>
+      <div className={styles.johnDoe}>John Doe</div>
+      <div className={styles.johnDoe1}>John Doe</div>
+      <div className={styles.johnDoe2}>John Doe</div>
+      <div className={styles.rectangleParent2}>
+        <div className={styles.groupChild14} />
+        <div className={styles.groupChild15} />
+        <div className={styles.groupChild16} />
+      </div>
+      <div className={styles.frameChild31} />
+      <div className={styles.frameChild32} />
+      <div className={styles.frameChild33} />
+      <div className={styles.frameChild34} />
+      <div className={styles.frameChild35} />
+      <div className={styles.frameChild36} />
+      <div className={styles.dropALine}>Drop a line</div>
+      <div className={styles.service}>24/7 Service</div>
+      <div className={styles.location}>Location</div>
+      <div className={styles.mailUs}>Mail Us</div>
+      <div className={styles.callUs}>Call Us</div>
+      <div className={styles.vistiUs}>Visti Us</div>
+      <img className={styles.groupIcon2} alt="" src="/group2.svg" />
+      <div className={styles.frameChild37} />
+      <div className={styles.frameChild38} />
+      <div className={styles.frameChild39} />
+      <div className={styles.frameChild40} />
+      <div className={styles.frameChild41} />
+      <div className={styles.name}>Name</div>
+      <div className={styles.mobileNo}>Mobile No</div>
+      <div className={styles.sendMessage1}>Send Message</div>
+      <div className={styles.email}>Email</div>
+      <div className={styles.subject}>Subject</div>
+      <div className={styles.frameChild42} />
+      <div className={styles.bitech1}>Bitech</div>
+      <div className={styles.loremIpsumIs7}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500shas been the industry's standard dummy text ever since
+        the 1500s
+      </div>
+      <img className={styles.frameChild43} alt="" src="/group-71.svg" />
+      <div className={styles.quickLinksHomeContainer}>
+        <p className={styles.quickLinks}>Quick Links</p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>Home</p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>About Us</p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>Gallery</p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>Contact</p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.siteMap}>Site Map</p>
+      </div>
+      <div className={styles.contactUsA37Container}>
+        <p className={styles.quickLinks}>Contact Us</p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>A-37, Sector 62, Noida-20</p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>090078601</p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>xyz@gmail.com</p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.siteMap}>&nbsp;</p>
+      </div>
+      <div className={styles.getHelpOurContainer}>
+        <p className={styles.quickLinks}>Get Help</p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>Our Team</p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>Partner</p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>FAQ</p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>Blog</p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.siteMap}>&nbsp;</p>
+      </div>
+      <div className={styles.frameChild44} />
+      <div className={styles.bitechGraphicsallRights}>
+        2023 Bitech Graphics.All rights reserved
+      </div>
+      <div className={styles.portfolioHowIt}>
+        Portfolio How it works Pricing About Login
+      </div>
+      <img className={styles.vectorIcon4} alt="" src="/vector2.svg" />
+      <img className={styles.vectorIcon5} alt="" src="/vector3.svg" />
+      <img className={styles.vectorIcon6} alt="" src="/vector4.svg" />
+      <div className={styles.support12gmailcomXyzgmail}>
+        <p className={styles.support12gmailcom}>Support12@gmail.com</p>
+        <p className={styles.wereADigital}>xyz@gmail.com</p>
+      </div>
+      <div className={styles.div4}>
+        <p className={styles.support12gmailcom}>09007861</p>
+        <p className={styles.wereADigital}>021-5698423</p>
+      </div>
+      <div className={styles.rafeloStHoustonykContainer}>
+        <p className={styles.support12gmailcom}>158 rafelo st,</p>
+        <p className={styles.wereADigital}>houston,yk 5964,uk</p>
+      </div>
+      <img className={styles.frameChild45} alt="" src="/group-75.svg" />
+      <img className={styles.octiconmail24} alt="" src="/octiconmail24.svg" />
+    </div>
+  );
+};
+
+export default FrameComponent;
